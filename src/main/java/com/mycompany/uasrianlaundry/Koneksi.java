30d7e22cccd15b04209fd6a3a295bfa263b3b330
unknown <a.riansyah2424@gmail.com>
2022-01-10 22:15:47 +0800

    UAS PBO RIANSYAH

---
+++ src/main/java/com/mycompany/uasrianlaundry/Koneksi.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package koneksi;
+import javax.swing.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author ahmdriansyh
+ */
+public class Koneksi {
+    private static Connection MySQLConfig;
+   public static Connection configDB()throws SQLException{
+       try{
+           String url = "jdbc:mysql://localhost:3306/uaspboriansyah";
+           String user = "root";
+           String pass = "";
+           
+           DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+           MySQLConfig = DriverManager.getConnection(url, user, pass);
+           
+       } catch (SQLException e) {
+           
+           System.out.println("KONEKSI KE DATABASE GAGAL " + e.getMessage());
+       }
+       return MySQLConfig;
+   }
+    
+}
